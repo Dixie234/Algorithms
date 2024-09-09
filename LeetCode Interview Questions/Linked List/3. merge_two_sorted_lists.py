01e1226463f75c054590ad8eb1915611707338f2
Dixie234 <52049242+Dixie234@users.noreply.github.com>
2024-09-09 21:41:16 +0100

    update to merge sort question

--- LeetCode Interview Questions/Linked List/3. merge_two_sorted_lists.py
+++ LeetCode Interview Questions/Linked List/3. merge_two_sorted_lists.py
@@ -15,11 +15,8 @@ def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optio
     
     tail = None
     start = None
-    while list1 or list2:
-        val1 = list1.val if list1 else float('inf')
-        val2 = list2.val if list2 else float('inf')
-
-        if val1 <= val2:
+    while list1 and list2:
+        if list1.val <= list2.val:
             node = list1
             list1 = list1.next if list1 else None
         else:
@@ -32,5 +29,10 @@ def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optio
         else:
             tail.next = node
             tail = tail.next
+
+    if list1:
+        tail.next = list1
+    else:
+        tail.next = list2
 
     return start

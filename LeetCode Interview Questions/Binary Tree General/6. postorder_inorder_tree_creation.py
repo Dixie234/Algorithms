7e0ad9045ad1495ad6b1c87c8f1598e18d72e5c3
Dixie234 <52049242+Dixie234@users.noreply.github.com>
2024-09-22 11:31:17 +0100

    add binary tree problems

--- LeetCode Interview Questions/Binary Tree General/6. postorder_inorder_tree_creation.py
+++ LeetCode Interview Questions/Binary Tree General/6. postorder_inorder_tree_creation.py
@@ -7,31 +7,30 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def buildTree(preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-    if not preorder:
+def buildTree(postorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    if not postorder:
         return None
     
-    preorder = deque(preorder)
     inorder = { val: index for index, val in enumerate(inorder) }
 
     def build(start:int, end:int):
         if start > end:
             return None
         
-        val = preorder.popleft()
+        val = postorder.pop()
         mid = inorder[val]
         node = TreeNode(val)
-
-        node.left = build(start, mid - 1)
+        
         node.right = build(mid + 1, end)
+        node.left = build(start, mid - 1)
 
         return node
             
 
     return build(0, len(inorder) - 1)
 
-preorder = [3,9,20,15,7]
 inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
 
-result = buildTree(preorder, inorder)
+result = buildTree(postorder, inorder)
 print(result)

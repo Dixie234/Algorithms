dee0906149506d47678611fff59a97735b10ac76
Guy Dixon <guy.dixon@creditbenchmark.com>
2025-01-10 18:39:57 +0000

    addition of trie delete

--- Trees/Trie.py
+++ Trees/Trie.py
@@ -8,6 +8,7 @@ from typing import List, Optional
 class TrieNode:
     def __init__(self):
         self.children = {}
+        self.end_node = False
 
 class Trie:
     def __init__(self):
@@ -35,13 +36,30 @@ class Trie:
                 curr.children[char] = newNode
                 curr = newNode
 
-        curr.children["*"] = None
+        curr.end_node = True
+
+    def delete(self, word:str) -> None:
+        curr = self.root
+        def dfs(curr:TrieNode, index=0):
+            if not curr.children:
+                return False
+            
+            result = dfs(curr.children[word[index]], index + 1)
+            if not result:
+                del curr.children[word[index]]
+            if not curr.children:
+                return False
+
+            return True
+        
+        dfs(curr)
+        
 
     def collect_all_words(self, node:TrieNode=None, word:str="", words:List[str]=[]) -> List[str]:
         curr = node or self.root
 
         for key, childNode in curr.children.items():
-            if key == "*":
+            if curr.end_node:
                 words.append(word)
             else:
                 self.collect_all_words(childNode, word + key, words)
@@ -54,3 +72,13 @@ class Trie:
             return None
         else:
             return self.collect_all_words(curr)
+        
+
+trie = Trie()
+trie.insert("rat")
+trie.insert("car")
+trie.insert("ran")
+trie.insert("the")
+trie.delete("rat")
+print(trie)
+    

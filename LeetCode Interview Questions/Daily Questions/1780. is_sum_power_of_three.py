d67162a85bdc5561526b6647ec75cf0bf74e9e6d
Guy Dixon <guy.dixon@creditbenchmark.com>
2025-03-04 17:26:45 +0000

    addition of daily problem

---
+++ LeetCode Interview Questions/Daily Questions/1780. is_sum_power_of_three.py
@@ -0,0 +1,22 @@
+import math
+
+#1, 3, 9, 27, 81, 243
+def checkPowersOfThree(n: int) -> bool:
+    used = set()
+    epsilon = 1e-9
+    while n != 0:
+        start = math.floor(math.log(n, 3) + epsilon) 
+        val = 3 ** start
+        if val in used:
+            return False
+        n -= val
+        used.add(val)
+        if n < 0:
+            return False
+    return True
+
+n = 6574365
+result = checkPowersOfThree(n)
+print(result)
+
+    
